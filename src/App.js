d8a915bd6905afebcd140b9c0a740b7a1a88f030
sulealaoglu <hsulealaoglu@gmail.com>
2024-03-03 15:36:46 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import LoginForm from "./Components/LoginForm/LoginForm";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
