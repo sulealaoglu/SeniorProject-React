f65fb1e896122c2930238c8bd9f3ec52cb0d2c18
sulealaoglu <hsulealaoglu@gmail.com>
2024-04-13 21:11:32 +0300

    ddvp fix

--- src/pages/Ddvp.js
+++ src/pages/Ddvp.js
@@ -4,24 +4,33 @@ import React, { useState } from "react";
 export default function Ddvp() {
   const [receivedText, setReceivedText] = useState("");
   const [sentimentResult, setSentimentResult] = useState("");
+  const [sentimentWords, setSentimentWords] = useState([]);
 
   const receiveText = (text) => {
     setReceivedText(text);
   };
 
-  const receiveSentimentResult = (result) => {
-    setSentimentResult(result);
+  const handleSentimentResultReceived = (result) => {
+    const wordListItems = result.words.map((wordObj, index) => (
+      <li key={index}>
+        <p>Word: {wordObj.word}</p>
+        <p>Positive: {wordObj.pos}</p>
+        <p>Negative: {wordObj.neg}</p>
+      </li>
+    ));
+    setSentimentResult(result.result);
+    setSentimentWords(wordListItems);
   };
 
   return (
     <div>
       <InputComponent
         onTextReceived={receiveText}
-        onSentimentResultReceived={receiveSentimentResult}
-      />{" "}
-      {/* Input bileşenini dahil ediyoruz ve onTextReceived prop'unu kullanıyoruz */}
+        onSentimentResultReceived={handleSentimentResultReceived}
+      />
       <p>Girilen Metin: {receivedText}</p>
       <p>Sentiment Sonucu: {sentimentResult}</p>
+      <ul>{sentimentWords}</ul>
     </div>
   );
 }
