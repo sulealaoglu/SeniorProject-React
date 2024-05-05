c15786ec02f5bd6a00d5622235cd9497daeaba4e
ceylinoznalbant <102251384+ceylinoznalbant@users.noreply.github.com>
2024-05-05 21:57:31 +0300

    profile page added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Pri } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Register from "./pages/Register.js";
-import Profile from "./pages/Profile";
+import Profile from "./pages/Profile.js";
 import DDVP from "./pages/Ddvp.js";
 import PrivateRoute from "./Components/PrivateRoute";
 import DasoForm from "./pages/Daso.js";
@@ -24,14 +24,14 @@ function App() {
         <Route path="/Takvim" element={<MoodCalendar />} />
         <Route path="/DDVP" element={<DDVP />} />
         <Route path="/Home" element={<Home />} />
-        <Route
-          path="/profile"
-          element={
+        <Route path="/Profile" element={<Profile />} />
+        
+        {/* <Route path="/profile" element={
             <PrivateRoute>
               <Profile />
             </PrivateRoute>
           }
-        />
+        />  */}
       </Routes>
     </>
   );
