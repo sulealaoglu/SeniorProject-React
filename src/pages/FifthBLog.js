b0778281a7622673e195d60a07a9de5b4e4288f2
ceylinoznalbant <102251384+ceylinoznalbant@users.noreply.github.com>
2024-05-16 20:40:51 +0300

    edit blog pages

--- src/pages/FifthBLog.js
+++ src/pages/FifthBLog.js
@@ -1,13 +1,34 @@
 
 import React from 'react';
 import './style.css';
+import blogPhoto from '../Components/Assests/blogs/Mukemmeliyetcilik.png';
+
 // https://www.hiwellapp.com/blog/mukemmelliyetcilik
 export default function FifthBlog() {
     
     return (
         <div className="blog-container">
             <h1>Kusursuza Ulaşma Çabası: Mükemmeliyetçilik Nedir? Altında Yatan Sebepler ve Etkileri</h1>
-            
+            <p>
+            Mükemmeliyetçilik, birçok insanın yaşamında yaygın olarak karşılaştığı ancak genellikle farkında olmadığı bir durumdur. Kendi belirlediği yüksek standartlara ulaşmak için sürekli çaba göstermek, mükemmeliyetçiliğin temel özelliğidir. Ancak bu durum, endişe, stres ve hatta kişinin kendisine zarar vermesiyle sonuçlanabilir. 
+            </p>
+   
+            <p>
+            Mükemmeliyetçilik, kişinin kendisi için belirlediği yüksek standartlara ulaşmak için sürekli olarak çabalamasıdır. Bu standartlar genellikle gerçekçi olmayabilir ve kişi, başkalarının değerlendirmelerine aşırı duyarlı olabilir.
+            </p>
+             <img src={blogPhoto} alt="Blog" />
+            <p>
+            Mükemmeliyetçilik belirtileri arasında gerçekçi olmayan standartlar belirleme, hata yapma ve başarısız olma korkusu, yoğun öz eleştiri, karar vermede güçlük gibi durumlar yer alır. Bu belirtiler, kişinin yaşamını olumsuz yönde etkileyebilir.  
+            </p>
+            <p>
+            Mükemmeliyetçilik genellikle aile ve yetiştirme tarzı, kültürel ve toplumsal beklentiler, kişilik özellikleri, erken dönem başarı ve başarısızlık deneyimleri, sosyal kıyaslama, düşük benlik saygısı gibi faktörlerden kaynaklanabilir. Bu faktörler, kişinin mükemmeliyetçi eğilimler geliştirmesine neden olabilir. 
+            </p>
+            <p>
+            Mükemmeliyetçilik, sadece kişinin kendisine zarar vermekle kalmaz, aynı zamanda psikolojik sorunlara da yol açabilir. Anksiyete, depresyon, beslenme bozuklukları gibi sorunlar mükemmeliyetçilikle sıkça ilişkilendirilir. Ayrıca mükemmeliyetçilik, erteleme ve kaçınma davranışlarına da yol açabilir, bu da kişinin potansiyelini gerçekleştirmesini engelleyebilir.
+            </p>
+            <p>
+            Mükemmeliyetçilik, hayatın birçok alanında karşımıza çıkan ancak çoğu zaman farkında olmadığımız bir durumdur. Ancak bu durum, kişinin yaşamını olumsuz yönde etkileyebilir ve psikolojik sorunlara yol açabilir. Bu nedenle, mükemmeliyetçilikle başa çıkmak ve sağlıklı bir yaşam sürmek için bilinçli bir çaba gereklidir.
+            </p>
         </div>
     );
 }
--- src/pages/FirstBlog.js
+++ src/pages/FirstBlog.js
@@ -6,23 +6,22 @@ export default function FirstBlog() {
     
             <div className="blog-container">
                 <h1>Güven Nasıl Öğrenilir? Kaygılı Bağlanma Nasıl Düzelir?</h1>
-                <p>
-                Güven, bir bireyin diğer insanlara veya durumlara karşı duyduğu korku, çekince ve kuşku olmaksızın inanma ve bağlanma duygusudur. Hayatın ilk yıllarında, bir bebek için güven duygusunun temelleri atılır. Güvenli veya güvensiz bağlanma modelleri, bu ilk yıllarda ebeveynlerin çocuklarına karşı tutumları ve davranışları ile şekillenir.
-                </p>
-                <p>
-Güvenli bağlanma, anne veya bakım verenin bebeğin duygusal ve fiziksel ihtiyaçlarına karşı duyarlı ve tutarlı bir şekilde yanıt vermesiyle oluşur. Bu, bebek için dünyanın güvenli bir yer olduğu ve kendisinin de sevilebilir olduğu inancını güçlendirir. Anne, çocuğun ihtiyaç duyduğu sevgi ve desteği sağladığında, çocuk hem kendini güvende hisseder hem de diğer insanlara güvenmeyi öğrenir. Güvenli bağlanan bireyler, zorluklarla karşılaştıklarında çözüm yolları bulma konusunda daha umutlu ve iyimser olabilirler.
-</p> 
-<img src={blogPhoto} alt="Blog" />
-<p>
-Öte yandan, güvensiz bağlanma genellikle ebeveynin tutarsız, duyarsız veya öngörülemeyen davranışları sonucunda ortaya çıkar. Bu durumda bebek, dünyanın güvenli bir yer olmadığı ve insanların güvenilmez olduğu inancını geliştirir. Bu tür bir bağlanma modeliyle büyüyen bireyler, ilerleyen yaşamlarında ilişkilerde güven sorunları yaşayabilir, yalnızlığa ve sosyal izolasyona daha yatkın olabilirler.
-</p>
-<p>
-Epigenetik, çevresel faktörlerin ve yaşam tarzı seçimlerinin gen ifadesini nasıl değiştirebileceğini gösteren bir bilim dalıdır. Bu, yetişkinlikte bile güvensiz bağlanma modellerinin üstesinden gelinip, daha sağlıklı ve güvenli ilişki dinamikleri oluşturulabileceği anlamına gelir. Değişim ve iyileşme, bireyin bilinçli çabaları ve çevresel destekle mümkün olabilir. Bu süreçte terapi, meditasyon, ve bilinçli farkındalık pratikleri gibi araçlar kullanılabilir.
-</p>
-<p>
-Sonuç olarak, güven öğrenilebilir ve geliştirilebilir bir duygudur. Bireyler, yaşam boyu süren bir süreçte güven duygularını yeniden değerlendirebilir ve güçlendirebilirler. Her bireyin geçmiş deneyimleri farklı olduğu için, güven geliştirmek için izlenecek yol da kişiye özel olacaktır. Ancak temelde, güvenli bir bağlanma deneyimi yaşamak ve bu deneyimi diğer ilişkilere taşıyabilmek, kişisel ve toplumsal iyilik halinin anahtarlarından biridir.
-</p>              
-                
+            <p>
+            İnsan ilişkilerinde güven ve bağlanma kavramları, hayati öneme sahiptir. Bu kavramlar, bir bireyin kendini başka birine güvenle bağlayabilmesi, duygusal sağlamlık kazanabilmesi ve sağlıklı ilişkiler kurabilmesi için gereklidir. Peki, güven ve bağlanma nasıl gelişir ve neden bazılarımız için daha zor olabilir?
+            </p>
+            <p>
+            İlk olarak, güvenli bağlanmanın temellerini ele alalım. Bir bebek için, annenin sunduğu duygusal ve fiziksel uyum, dünyayı güvenli ve sevgi dolu bir yer olarak algılamasını sağlar. Bu güvenli bağlanma, ileriki ilişkilerinde de temel bir referans noktası olacaktır. Ancak, bazı durumlarda bu uyum sağlanamaz ve güvensiz bağlanma gelişir.
+            </p>
+            <img src={blogPhoto} alt="Blog" />
+            <p>
+            Güvensiz bağlanma, genellikle anne ile bebeğin uyumsuzluğu veya annenin yetersiz destek sağlayabilmesi durumunda ortaya çıkar. Bu durumda, bebek çevresini güvensiz ve kaygı verici bir yer olarak algılar. Bu da, ileriki ilişkilerinde güven sorunları yaşamasına neden olabilir.
+            </p>             
+            <p>
+            Peki, güven ve bağlanma nasıl geliştirilebilir? İşte burada epigenetik devreye girer. Epigenetik, çevresel etkenlerin gen ifadesini nasıl etkileyebileceğini gösterir. Yani, yaşam tarzı faktörleri ve çevresel etkenler, güven ve bağlanma üzerinde belirleyici bir rol oynar. Bu da demektir ki, kişinin yaşamının kontrolü tamamen dış etkenler tarafından belirlenmez; aksine, kişi bu etkenler üzerinde değişiklik yapabilir ve ilişkilerinde güveni ve bağlanmayı geliştirebilir.  
+            </p>
+            <p>
+            Sonuç olarak, güven ve bağlanma ilişkilerimizin temel taşlarıdır. Bu kavramları anlamak ve geliştirmek, sağlıklı ve tatmin edici ilişkiler kurmamıza yardımcı olacaktır. İçsel güven ve dışsal uyum, birbirini tamamlayarak, hayatımızı daha anlamlı hale getirebilir. 
+            </p>    
             </div>
      
     );
